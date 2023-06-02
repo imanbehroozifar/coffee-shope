ceb2eb638d0256502c876e1e99aae3fdf6519cb3
Iman Behroozifar <105700046+imanbehroozifar@users.noreply.github.com>
2023-06-02 23:49:01 +0430

    Configuring tailwindcss files and customizing them and add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "brown": {
+          100: "#ECE0d1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832"
+        }
       },
+      boxShadow: {
+        "shadow-normal": "0px 1px 10px rgba(0,0,0,0.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem"
+      },
+      fontFamily: {
+        "Dana": "Dana",
+        "DanaMedium": "Dana Medium",
+        "DanaDemiBold": "Dana DemiBold",
+        "MorabbaLigth":"Morabba Ligth",
+        "MorabbaMedium":"Morabba Medium",
+        "MorabbaBold":"Morabba Bold",
+      }
     },
   },
   plugins: [],
